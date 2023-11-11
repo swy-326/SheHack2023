98ce77605d3c1fc42a71cb94c189a6fbb56784ee
GiulianaPola <92754890+GiulianaPola@users.noreply.github.com>
2023-11-11 18:04:54 +0000

    criação dos campos latitude e longitude

--- teste.py
+++ teste.py
@@ -2,3 +2,21 @@ import geopandas as gpd
 
 gdf = gpd.read_file('EPSG900913.shp')
 print(gdf['eq_enderec'].iloc[0])
+import folium
+lat=[]
+lon=[]
+from geopy.geocoders import Nominatim
+
+for i in range(len(gdf)):
+    end=gdf['eq_enderec'].iloc[i]
+    # Obter as coordenadas a partir do endereço
+    geolocator = Nominatim(user_agent="my_app")
+    try:
+        location = geolocator.geocode(end)
+        lat.append(location.latitude)
+        lon.append(location.longitude)
+    except:
+        lat.append("")
+        lon.append("")
+gdf["latitude"]=lat
+gdf["longitude"]=lon
